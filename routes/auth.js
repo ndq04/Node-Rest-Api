2bd443716987d5d3d0c7ab9bd9212a0a8cffb4cc
ndq04 <duyquang412000@gmail.com>
2021-11-27 11:33:47 +0700

    edit auth.js file

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
-const bcrypt = require('bcrypt')
+const argon2 = require('argon2')
 
 // REGISTER
 router.post('/register', async (req, res) => {
   const {username, email, password} = req.body
 
-  if (!username || !password) {
+  if (!username) {
     return res.status(400).json({
       success: false,
-      massage: 'Missing username or/and password',
+      massage: 'Missing username !',
+    })
+  }
+  if (!email) {
+    return res.status(400).json({
+      success: false,
+      massage: 'Missing email !',
+    })
+  }
+  if (!password) {
+    return res.status(400).json({
+      success: false,
+      massage: 'Missing password !',
     })
   }
 
@@ -34,8 +46,7 @@ router.post('/register', async (req, res) => {
     }
 
     // Generate new password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
+    const hashedPassword = await argon2.hash(password)
 
     // Create new user
     const newUser = await new User({
@@ -48,13 +59,14 @@ router.post('/register', async (req, res) => {
     const user = await newUser.save()
     res.status(200).json({
       success: true,
-      message: 'Successful !',
+      message: 'Account created successfully !',
       user,
     })
   } catch (err) {
     res.status(500).json({
       success: false,
-      message: err.message,
+      message: 'Internal server error !',
+      error: err.message,
     })
   }
 })
@@ -62,36 +74,45 @@ router.post('/register', async (req, res) => {
 // LOGIN
 router.post('/login', async (req, res) => {
   const {email, password} = req.body
-  if (!email || !password) {
+  if (!email) {
     return res.status(400).json({
       success: false,
-      massage: 'Missing email or/and password',
+      massage: 'Missing email !',
+    })
+  }
+  if (!password) {
+    return res.status(400).json({
+      success: false,
+      massage: 'Missing password !',
     })
   }
   try {
     const user = await User.findOne({email})
-    !user &&
-      res.status(404).json({
+    if (!user) {
+      return res.status(400).json({
         success: false,
-        message: 'User not found !',
+        message: 'Incorrect email or password !',
       })
+    }
 
-    const validPassword = await bcrypt.compare(password, user.password)
-    !validPassword &&
-      res.status(400).json({
+    const validPassword = await argon2.verify(user.password, password)
+    if (!validPassword) {
+      return res.status(400).json({
         success: false,
-        message: 'Wrong password!',
+        message: 'Incorrect email or password !',
       })
+    }
 
     res.status(200).json({
       success: true,
-      message: 'Successful!',
+      message: 'Logged in successfully !',
       user,
     })
   } catch (err) {
     res.status(500).json({
       success: false,
-      message: err.message,
+      message: 'Internal server error !',
+      error: err.message,
     })
   }
 })
